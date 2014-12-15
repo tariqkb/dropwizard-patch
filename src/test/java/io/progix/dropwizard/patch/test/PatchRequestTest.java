3b5a79dd21078e75c69ffc02c4f7756bbdbb394d
Tariq Bugrara <tbugrara@gmail.com>
2014-12-15 08:59:51 -0500

    Added better unit testing for json deserialization

--- src/test/java/io/progix/dropwizard/patch/test/PatchRequestTest.java
+++ src/test/java/io/progix/dropwizard/patch/test/PatchRequestTest.java
@@ -19,21 +19,52 @@ public class PatchRequestTest {
 
     @Test
     public void deserializesFromJSON() throws Exception {
-        PatchInstruction add = new PatchInstruction(PatchOperation.ADD, "/a/b/c",
-                new ArrayList<Object>(Arrays.asList(42)), null);
-        PatchInstruction addElement = new PatchInstruction(PatchOperation.ADD, "/a/b/1",
-                new ArrayList<Object>(Arrays.asList(42)), null);
+        String one = "1";
+        Integer three = 3;
+        String[] s = new String[] {"1", "2"};
+        Integer[] i = new Integer[] {3, 4};
 
-        PatchInstruction replace = new PatchInstruction(PatchOperation.REPLACE, "/a/b/c",
-                new ArrayList<Object>(Arrays.asList(42)), null);
+        PatchInstruction r1 = replace("/a/c", "1");
+        PatchInstruction r2 = replace("/a/c", 3);
+        PatchInstruction r3 = replace("/a/c", s);
+        PatchInstruction r4 = replace("/a/c", i);
 
-        PatchInstruction remove = new PatchInstruction(PatchOperation.REMOVE, "/a/b/c", null, null);
-        PatchInstruction removeElement = new PatchInstruction(PatchOperation.REMOVE, "/a/b/1", null, null);
+        PatchInstruction r5 = replace("/a/0", "1");
+        PatchInstruction r6 = replace("/a/0", "1");
+        PatchInstruction r7 = replace("/a/0", "1");
+        PatchInstruction r8 = replace("/a/0", "1");
 
-        PatchInstruction test = new PatchInstruction(PatchOperation.TEST, "/a/b/c",
-                new ArrayList<Object>(Arrays.asList("C", "D")), null);
+        PatchInstruction r9 = replace("/a/-", "1");
+        PatchInstruction r10 = replace("/a/-", "1");
+        PatchInstruction r11 = replace("/a/-", "1");
+        PatchInstruction r12 = replace("/a/-", "1");
 
-        PatchRequest request = new PatchRequest(Arrays.asList(replace, test));
+        PatchRequest request = new PatchRequest(Arrays.asList(replace("/a/c", "1"), replace("/a/c", 3)));
+
         assertThat(MAPPER.readValue(fixture("fixtures/patchrequest.json"), PatchRequest.class)).isEqualTo(request);
+    }
+
+    private PatchInstruction replace(String path, Object... values) {
+        return new PatchInstruction(PatchOperation.REPLACE, path, new ArrayList<>(Arrays.asList(values)), null);
+    }
+
+    private PatchInstruction add(String path, Object... values) {
+        return new PatchInstruction(PatchOperation.ADD, path, new ArrayList<>(Arrays.asList(values)), null);
+    }
+
+    private PatchInstruction test(String path, Object... values) {
+        return new PatchInstruction(PatchOperation.TEST, path, new ArrayList<>(Arrays.asList(values)), null);
+    }
+
+    private PatchInstruction move(String path, String from) {
+        return new PatchInstruction(PatchOperation.MOVE, path, new ArrayList<>(), from);
+    }
+
+    private PatchInstruction copy(String path, String from) {
+        return new PatchInstruction(PatchOperation.COPY, path, new ArrayList<>(), from);
+    }
+
+    private PatchInstruction remove(String path) {
+        return new PatchInstruction(PatchOperation.COPY, path, new ArrayList<>(), null);
     }
 }
